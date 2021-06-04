e238e100b6c65cc64030054f9b264e39944cad63
Blake <btdubbzzz@gmail.com>
2021-06-04 00:46:39 -0500

    finished very basic app/question flow through inquirer

--- cms.js
+++ cms.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const questions = require('./src/questions');
 
 const connection = mysql.createConnection({
 	host: 'localhost',
@@ -8,3 +9,106 @@ const connection = mysql.createConnection({
 	password: 'password',
 	database: 'employees_db',
 });
+
+function homePage() {
+	inquirer.prompt(questions.homePageQuestions).then((res) => {
+		const answer = res.homeChoice;
+		switch (answer) {
+			case 'Add department':
+				addDepartmentPrompts();
+				break;
+			case 'Add role':
+				addRolePrompts();
+				break;
+			case 'Add employee':
+				addEmployeePrompts();
+				break;
+			case 'View departments':
+				viewDepartments();
+				break;
+			case 'View roles':
+				viewRoles();
+				break;
+			case 'View all employees':
+				viewAllEmployees();
+				break;
+			case 'View all employees by manager':
+				viewEmployeesByManager();
+				break;
+			case 'Update employee roles':
+				updateEmployeeRole();
+				break;
+			case 'Remove employee':
+				removeEmployee();
+				break;
+			case 'Exit':
+				break;
+		}
+	});
+}
+
+function addDepartmentPrompts() {
+	console.log('add d');
+	inquirer
+		.prompt(questions.addDepartmentQuestions)
+		.then((res) => console.log('res :>> ', res))
+		.then(function () {
+			homePage();
+		});
+}
+function addRolePrompts() {
+	console.log('add r');
+	inquirer
+		.prompt(questions.addRoleQuestions)
+		.then((res) => console.log('res :>> ', res))
+		.then(function () {
+			homePage();
+		});
+}
+function addEmployeePrompts() {
+	console.log('add e');
+	inquirer
+		.prompt(questions.addEmployeeQuestions)
+		.then((res) => console.log('res :>> ', res))
+		.then(function () {
+			homePage();
+		});
+}
+function viewDepartments() {
+	console.log('v d');
+}
+function viewRoles() {
+	console.log('v r');
+}
+function viewAllEmployees() {
+	console.log('v e');
+}
+function viewEmployeesByManager() {
+	console.log('v e b m');
+	inquirer
+		.prompt(questions.employeesByManager)
+		.then((res) => console.log('res :>> ', res))
+		.then(function () {
+			homePage();
+		});
+}
+function updateEmployeeRole() {
+	console.log('u r');
+	inquirer
+		.prompt(questions.updateRoleQuestions)
+		.then((res) => console.log('res :>> ', res))
+		.then(function () {
+			homePage();
+		});
+}
+function removeEmployee() {
+	console.log('r e');
+	inquirer
+		.prompt(questions.removeEmployeeQuestions)
+		.then((res) => console.log('res :>> ', res))
+		.then(function () {
+			homePage();
+		});
+}
+
+homePage();
---
+++ src/questions.js
@@ -0,0 +1,106 @@
+const homePageQuestions = [
+	{
+		message: 'What would you like to do?',
+		type: 'list',
+		name: 'homeChoice',
+		choices: [
+			'Add department',
+			'Add role',
+			'Add employee',
+			'View departments',
+			'View roles',
+			'View all employees',
+			'View all employees by manager',
+			'Update employee roles',
+			'Remove employee',
+			'Exit',
+		],
+	},
+];
+
+const addDepartmentQuestions = [
+	{
+		message: 'Please enter the name of the department',
+		type: 'input',
+		name: 'departmentName',
+	},
+];
+const addRoleQuestions = [
+	{
+		message: 'Please enter name of new role',
+		type: 'input',
+		name: 'roleName',
+	},
+	{
+		message: 'Please enter salary of this role',
+		type: 'input',
+		name: 'roleSalary',
+	},
+	{
+		message: 'Please choose the department for this role',
+		type: 'list',
+		name: 'roleDepartmentID',
+		choices: ['get list of departments'],
+	},
+];
+const addEmployeeQuestions = [
+	{
+		message: 'Please enter the first name of the employee',
+		type: 'input',
+		name: 'employeeFirstName',
+	},
+	{
+		message: 'Please enter the last name of the employee',
+		type: 'input',
+		name: 'employeeLastName',
+	},
+	{
+		message: 'Please choose employee role',
+		type: 'list',
+		name: 'employeeRole',
+		choices: ['get list of roles'],
+	},
+	{
+		message: "Please choose the employee's manager",
+		type: 'list',
+		name: 'employeeManager',
+		choices: ['get list of managers'],
+	},
+];
+
+const employeesByManager = [
+	{
+		message: 'Choose the Manager to see their employees',
+		type: 'list',
+		name: 'managerOfEmployees',
+		choices: ['get list of managers'],
+	},
+];
+
+const updateRoleQuestions = [
+	{
+		message: 'Please choose the employee to update their role',
+		type: 'list',
+		name: 'employeeToUpdate',
+		choices: ['get list of employees'],
+	},
+];
+
+const removeEmployeeQuestions = [
+	{
+		message: 'Please choose the employee to remove',
+		type: 'list',
+		name: 'employeeToRemove',
+		choices: ['get list of employees'],
+	},
+];
+
+module.exports = {
+	homePageQuestions,
+	addDepartmentQuestions,
+	addRoleQuestions,
+	addEmployeeQuestions,
+	employeesByManager,
+	updateRoleQuestions,
+	removeEmployeeQuestions,
+};
